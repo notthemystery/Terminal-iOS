236995f61dbc1ab11af0fadf05ab23bc2971c56d
The Mystery <132539789+notthemystery@users.noreply.github.com>
2026-05-16 20:06:13 +0530

    Update spawn.h

--- spawn.h
+++ spawn.h
@@ -1,13 +1,13 @@
-#ifndef spawn_h
-#define spawn_h
+#ifndef SPAWN_H
+#define SPAWN_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <stddef.h>
+#include <stdio.h>
 
-int run_command(const char *toolPath, const char *cmd, char **output);
+int run_command(const char *toolPath, char *const argv[], char **output);
 
 #ifdef __cplusplus
 }
